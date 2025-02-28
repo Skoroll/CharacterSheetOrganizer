f09361cb14c3da12304b7b579d86579afdbf9f85
Yann Gicquel <yg.gicquel@gmail.com>
2025-02-28 21:32:05 +0100

    Ajout du port et allowedHost

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
+/// <reference types="node" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const PORT = Number(process.env.PORT) || 4173;
 
 export default defineConfig({
   plugins: [react()],
@@ -8,7 +11,10 @@ export default defineConfig({
     alias: {
       src: "/src"
     }
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: PORT,  // ✅ Utilisation correcte du port
+    allowedHosts: ["charactersheetorganizer-1.onrender.com"] // ✅ URL Render autorisée
   }
 });
-
-
