c8a36a330584df41934868463c89e8a22613c478
Yann Gicquel <yg.gicquel@gmail.com>
2025-03-13 18:15:53 +0100

    Ajustement de landing

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,12 +1,12 @@
 
 import "./Landing.scss";
-
+import News from "../Articles/News";
 
 
 export default function Landing () {
     return (
         <div className="landing">
-
+            <News />
         </div>
     )
 }
